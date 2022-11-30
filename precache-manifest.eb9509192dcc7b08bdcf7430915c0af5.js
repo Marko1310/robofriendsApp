3a44730bdbcdc0ce52fd1db8173e5e4cd2cd8210
Marko1310 <marko.cabo13@gmail.com>
2022-11-30 01:54:41 +0100

    Updates

---
+++ precache-manifest.eb9509192dcc7b08bdcf7430915c0af5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8be4b29fbbe8c2034297",
+    "url": "/robofriendsApp/static/css/main.00280949.chunk.css"
+  },
+  {
+    "revision": "8be4b29fbbe8c2034297",
+    "url": "/robofriendsApp/static/js/main.8be4b29f.chunk.js"
+  },
+  {
+    "revision": "84cf99745ce0980187aa",
+    "url": "/robofriendsApp/static/js/1.84cf9974.chunk.js"
+  },
+  {
+    "revision": "9dedd7a5a9fb299ec63c",
+    "url": "/robofriendsApp/static/css/2.9a9052f2.chunk.css"
+  },
+  {
+    "revision": "9dedd7a5a9fb299ec63c",
+    "url": "/robofriendsApp/static/js/2.9dedd7a5.chunk.js"
+  },
+  {
+    "revision": "43007e5b8211023170cc",
+    "url": "/robofriendsApp/static/js/runtime~main.43007e5b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriendsApp/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "652cde54175f188246ccd09453d75448",
+    "url": "/robofriendsApp/index.html"
+  }
+];
